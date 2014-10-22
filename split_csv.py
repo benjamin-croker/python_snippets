480337dfdb151075a09aed298e91bf2c3f5c3588
Benjamin Croker <benjamin.croker@gmail.com>
2014-10-22 15:33:30 +1000

    Update split_csv.py

--- split_csv.py
+++ split_csv.py
@@ -1,12 +1,13 @@
-# Takes a large CSV file and splits it into individual worksheets of an Excel workbook
-
+# Takes a large CSV file and splits it into individual Excel workbooks
+ 
 import csv
 import itertools
 import sys
-
+import os
+ 
 from openpyxl import Workbook
-
-
+ 
+ 
 def split_csv_generator(reader, split_size):
     header = reader.next()
     while True:
@@ -17,35 +18,28 @@ def split_csv_generator(reader, split_size):
             break
     
     
-def split_into_workbook_sheets(split_gen, ws_prefix):
-    wb = Workbook()
+def split_into_workbook_sheets(split_gen, out_dir, ws_prefix):
     for i, (header, row) in enumerate(split_gen):
         # there will be one sheet when the workbook is created
-        if i == 0:
-            ws = wb.get_active_sheet()
-        else:
-            ws = wb.create_sheet()
+        wb = Workbook()
+        ws = wb.get_active_sheet()
         ws.title = '{}_{}'.format(ws_prefix, i)
         ws.append(header)
         for r in row:
             ws.append(r)
-            
-    return wb
+        wb.save(os.path.join(out_dir, '{}.xlsx'.format(ws.title)))
     
         
-def split(in_filename, out_filename, split_size, ws_prefix):
+def split(in_filename, out_dir, split_size, ws_prefix):
     reader = csv.reader(open(in_filename, 'rb'))
     split_gen = split_csv_generator(reader, split_size)
-    
-    wb = split_into_workbook_sheets(split_gen, ws_prefix)
-    wb.save(out_filename)
+    split_into_workbook_sheets(split_gen, out_dir, ws_prefix)
     
     
 if __name__ == '__main__':
-
+ 
     if len(sys.argv) != 5:
-        print("Usage:\tsplit_csv.py input_file output_file block_size worksheet_prefix")
+        print("Usage:\tsplit_csv.py input_file output_directory block_size worksheet_prefix")
     
     else:
         split(sys.argv[1], sys.argv[2], int(sys.argv[3]), sys.argv[4])
-        
